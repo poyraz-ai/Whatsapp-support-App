3054aa5d693335bb842ebf1982b4ae872cbb6411
poyraz-ai <tekyildirimpoyraz7@gmail.com>
2026-01-14 16:19:48 +0100

    Add whatsapp js

---
+++ whatsapp.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export async function sendWhatsAppMessage(to, text) {
+  await axios.post(
+    `https://graph.facebook.com/v19.0/${process.env.PHONE_NUMBER_ID}/messages`,
+    {
+      messaging_product: "whatsapp",
+      to,
+      text: { body: text }
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
