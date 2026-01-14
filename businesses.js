555ac7208815af58738f57b9255d585596b70e99
poyraz-ai <tekyildirimpoyraz7@gmail.com>
2026-01-14 16:21:14 +0100

    Add business.js

---
+++ businesses.js
@@ -0,0 +1,19 @@
+export const BUSINESSES = {
+  "default": {
+    name: "Muster Geschäft",
+    tone: "freundlich, professionell",
+    opening_hours: "Mo–Fr 9–18 Uhr",
+    services: [
+      "Beratung",
+      "Terminvereinbarung",
+      "Allgemeiner Support"
+    ],
+    handover_keywords: [
+      "beschwerde",
+      "reklamation",
+      "anwalt",
+      "unzufrieden"
+    ],
+    human_contact: "Ein Mitarbeiter meldet sich zeitnah bei dir 👍"
+  }
+};
